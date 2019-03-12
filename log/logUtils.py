1d6cd352cb7ce9dee949d18c08cd81225a4abe68
Vipsu <randoomlp@gmail.com>
2019-03-12 09:55:29 +0100

    Hoshio

--- log/logUtils.py
+++ log/logUtils.py
@@ -3,11 +3,12 @@ from common import generalUtils
 from objects import glob
 from common.ripple import userUtils
 import time
+import sys
 import os
 
 ENDL = "\n" if os.name == "posix" else "\r\n"
 
-def logMessage(message, alertType = "INFO", messageColor = bcolors.ENDC, discord = None, alertDev = False, of = None, stdout = True):
+def logMessage(message, alertType = "INFO", messageColor = bcolors.ENDC, discord = None, of = None, stdout = True):
 	"""
 	Log a message
 
@@ -15,16 +16,13 @@ def logMessage(message, alertType = "INFO", messageColor = bcolors.ENDC, discord
 	:param alertType: alert type string. Can be INFO, WARNING, ERROR or DEBUG. Default: INFO
 	:param messageColor: message console ANSI color. Default: no color
 	:param discord: Discord channel acronym for Schiavo. If None, don't log to Discord. Default: None
-	:param alertDev: 	if True, developers will be highlighted on Discord.
-						Obviously works only if the message will be logged to Discord.
-						Default: False
 	:param of:	Output file name (inside .data folder). If None, don't log to file. Default: None
 	:param stdout: If True, log to stdout (print). Default: True
 	:return:
 	"""
 	# Get type color from alertType
 	if alertType == "INFO":
-		typeColor = bcolors.GREEN
+		typeColor = bcolors.CYAN
 	elif alertType == "WARNING":
 		typeColor = bcolors.YELLOW
 	elif alertType == "ERROR":
@@ -32,6 +30,8 @@ def logMessage(message, alertType = "INFO", messageColor = bcolors.ENDC, discord
 	elif alertType == "CHAT":
 		typeColor = bcolors.BLUE
 	elif alertType == "DEBUG":
+		typeColor = bcolors.PINK
+	elif alertType == "CMYUI":
 		typeColor = bcolors.PINK
 	else:
 		typeColor = bcolors.ENDC
@@ -44,7 +44,6 @@ def logMessage(message, alertType = "INFO", messageColor = bcolors.ENDC, discord
 		time=generalUtils.getTimestamp(),
 		type=alertType,
 		message=message,
-
 		typeColor=typeColor,
 		messageColor=messageColor,
 		endc=bcolors.ENDC)
@@ -52,11 +51,12 @@ def logMessage(message, alertType = "INFO", messageColor = bcolors.ENDC, discord
 	# Log to console
 	if stdout:
 		print(finalMessageConsole)
+		sys.stdout.flush()
 
 	# Log to discord if needed
 	if discord is not None:
 		if discord == "bunker":
-			glob.schiavo.sendConfidential(message, alertDev)
+			glob.schiavo.sendConfidential(message)
 		elif discord == "cm":
 			glob.schiavo.sendCM(message)
 		elif discord == "staff":
@@ -68,44 +68,35 @@ def logMessage(message, alertType = "INFO", messageColor = bcolors.ENDC, discord
 	if of is not None:
 		glob.fileBuffers.write(".data/"+of, finalMessage+ENDL)
 
-def warning(message, discord = None, alertDev = False):
+def warning(message, discord = None):
 	"""
 	Log a warning to stdout and optionally to Discord
 
 	:param message: warning message
 	:param discord: Discord channel acronym for Schiavo. If None, don't log to Discord. Default: None
-	:param alertDev: 	if True, developers will be highlighted on Discord.
-						Obviously works only if the message will be logged to Discord.
-						Default: False
 	:return:
 	"""
-	logMessage(message, "WARNING", bcolors.YELLOW, discord, alertDev)
+	logMessage(message, "WARNING", bcolors.YELLOW, discord)
 
-def error(message, discord = None, alertDev = True):
+def error(message, discord = None):
 	"""
 	Log a warning message to stdout and optionally to Discord
 
 	:param message: warning message
 	:param discord: Discord channel acronym for Schiavo. If None, don't log to Discord. Default: None
-	:param alertDev: 	if True, developers will be highlighted on Discord.
-						Obviously works only if the message will be logged to Discord.
-						Default: False
 	:return:
 	"""
-	logMessage(message, "ERROR", bcolors.RED, discord, alertDev)
+	logMessage(message, "ERROR", bcolors.RED, discord)
 
-def info(message, discord = None, alertDev = False):
+def info(message, discord = None):
 	"""
 	Log an info message to stdout and optionally to Discord
 
 	:param message: info message
 	:param discord: Discord channel acronym for Schiavo. If None, don't log to Discord. Default: None
-	:param alertDev: 	if True, developers will be highlighted on Discord.
-						Obviously works only if the message will be logged to Discord.
-						Default: False
 	:return:
 	"""
-	logMessage(message, "INFO", bcolors.ENDC, discord, alertDev)
+	logMessage(message, "INFO", bcolors.ENDC, discord)
 
 def debug(message):
 	"""
@@ -129,14 +120,23 @@ def chat(message):
 
 def pm(message):
 	"""
-	Log a private chat message to stdout. Currently not used.
+	Log a private chat message to stdout and to chatlog_private.txt.
 
 	:param message: message content
 	:return:
 	"""
-	logMessage(message, "CHAT", bcolors.BLUE)
+	logMessage(message, "CMYUI", bcolors.CYAN, of="chatlog_private.txt")
 
-def rap(userID, message, discord=False, through=None):
+def hoshio(message, discord=None):
+	"""
+	Just a log that is meant to stand out in console. Meant for testing things, generally..
+
+	:param message: message content
+	:return:
+	"""
+	logMessage(message, "HOSHIO", bcolors.CYAN, discord)
+
+def rap(userID, message, discord=False, through="Charlotte"):
 	"""
 	Log a message to Admin Logs.
 
@@ -146,9 +146,6 @@ def rap(userID, message, discord=False, through=None):
 	:param through: through string. Default: FokaBot
 	:return:
 	"""
-	if through is None: #Set default messager to bot account
-		through = glob.BOT_NAME
-
 	glob.db.execute("INSERT INTO rap_logs (id, userid, text, datetime, through) VALUES (NULL, %s, %s, %s, %s)", [userID, message, int(time.time()), through])
 	username = userUtils.getUsername(userID)
 	logMessage("{} {}".format(username, message), discord=True)
